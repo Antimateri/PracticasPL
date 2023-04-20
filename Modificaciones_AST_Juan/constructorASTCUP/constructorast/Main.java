5479811e95b9ba9d4aa1c97a3f58b12599060b5d
PabloHG01 <pahere01@ucm.es>
2023-04-20 21:37:45 +0200

    calcula bien las deltas (a falta de vincular) pero falta para funciones

--- Modificaciones_AST_Juan/constructorASTCUP/constructorast/Main.java
+++ Modificaciones_AST_Juan/constructorASTCUP/constructorast/Main.java
@@ -4,14 +4,17 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import alex.AnalizadorLexicoExp;
+import ast.*;
 
 public class Main {
    public static void main(String[] args) throws Exception {
      Reader input = new InputStreamReader(new FileInputStream(args[0]));
 	 AnalizadorLexicoExp alex = new AnalizadorLexicoExp(input);
 	 ConstructorASTExp constructorast = new ConstructorASTExp(alex);
-	 //constructorast.parse();
-	 System.out.println(constructorast.parse().value);
+	 Statement raiz = (Statement)constructorast.parse().value;
+	 //System.out.println(constructorast.parse().value);
+	 raiz.setDelta(0);
+	 System.out.println(raiz);
  }
 }   
    
